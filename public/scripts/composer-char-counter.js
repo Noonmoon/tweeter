31188e7f7e178c19074018a283f7a592e57daff3
Tristan <tristandeering@hotmail.com>
2018-11-08 00:13:43 +0000

    first commit - finished layout and dynamically loading hardcoded tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.textbox.input').on('keyup', function() {
+    var value = 140 - ($(this).val().length)
+    var counter = $(this).parent().children('span.counter');
+    counter.html(value)
+
+    if (value < 0) {
+      counter.css({color: 'red'})
+    } else if (value > 0) {
+      counter.css({color: '#244751'})
+    }
+  });
+});
+
---
+++ public/scripts/hover.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.tweets.box').hover(function() {
+    $(this).css({opacity: 1})
+  }, function() {
+    $(this).css({opacity: 0.7})
+  });
+});
